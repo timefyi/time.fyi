ea0d6af1ab32df4988f8fe6a7d0cf41608481c28
Kamran Ahmed <kamranahmed.se@gmail.com>
2019-06-21 15:33:41 +0400

    Update depenedencies and keywords

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 'use strict';
 const meow = require('meow');
-const path = require('path');
 const React = require('react');
 const { render } = require('ink');
 const importJsx = require('import-jsx');
